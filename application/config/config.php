0f185617f22850229887bd919a77b9b06b65bd04
Lansana <lansana@localhost.localdomain>
2018-05-23 19:21:43 +0200

    23/05/2018 Config deploy

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/mycode/bit-maths/';
+$config['base_url'] = 'https://lansana.randion.es/bit-maths/';
 
 /*
 |--------------------------------------------------------------------------
